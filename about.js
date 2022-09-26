2c718915608dab995b0e4445e8401a41ace8ed64
PriyaDeveloper630 <priyamalik630@gmail.com>
2022-09-26 21:09:06 +0530

    updates

---
+++ about.js
@@ -0,0 +1,45 @@
+var rot = 0,
+    con = document.querySelector('#caro_con'),
+    btns = document.querySelectorAll('button'),
+    tran_time = 1000
+
+btns.forEach(function(btn) {
+  btn.addEventListener('click', function(){
+    if(btn.className == 'btn_left') {
+      rot = rot - 90
+      con.style.transform = 'rotate('+rot+'deg)'
+    }
+    if(btn.className == 'btn_right') {
+      rot = rot + 90
+      con.style.transform = 'rotate('+rot+'deg)'
+    }
+  })
+})
+
+window.addEventListener('keyup', function(e){
+  if(e.keyCode === 37) {
+    rot = rot - 90
+    con.style.transform = 'rotate('+rot+'deg)'
+  }
+  if(e.keyCode === 39) {
+    rot = rot + 90
+    con.style.transform = 'rotate('+rot+'deg)'
+  }
+})
+
+function mouseWheel(e) {
+  // console.log(e)
+  window.removeEventListener('wheel', mouseWheel)
+  setTimeout(function(){
+    window.addEventListener('wheel', mouseWheel)
+  }, tran_time)
+
+  if(e.deltaY < 0) {    
+    rot = rot - 90
+    con.style.transform = 'rotate('+rot+'deg)'
+  } else {
+    rot = rot + 90
+    con.style.transform = 'rotate('+rot+'deg)'
+  }
+}
+window.addEventListener('wheel', mouseWheel)
